e81bfc5a6d884c934cc4e7131d18fdae3130d030
lionliliya <liliya_yalovchenko@ukr.net>
2016-01-31 14:23:10 +0200

    moving Validator to src

---
+++ src/MatrixValidator.java
@@ -0,0 +1,40 @@
+package com.company;
+
+/**
+ * Created by lionliliya on 27.01.16.
+ */
+public class MatrixValidator {
+
+    public static boolean areMultiplyable(Matrix m1, Matrix m2) {
+
+        return m1.getColumnsNumber() == m2.getRowsNumber();
+
+    }
+
+    public static boolean areSameDimension(Matrix m1, Matrix m2) {
+
+        return ((m1.getRowsNumber() == m2.getRowsNumber()) && (m1.getColumnsNumber() == m2.getColumnsNumber()));
+
+    }
+
+
+    public static boolean isDigit(String userInput) {
+        try {
+            Double.parseDouble(userInput);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+    }
+
+    public static boolean isInteger(String userInput) {
+        try {
+            Integer.parseInt(userInput);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+}
