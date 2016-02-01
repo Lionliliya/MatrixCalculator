d1ca97b8554cf73212736e9322947400c8bceb78
lionliliya <liliya_yalovchenko@ukr.net>
2016-02-01 15:36:22 +0200

    tests commit

---
+++ tests/MatrixTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MatrixTest {
+
+    @Test
+    public void testGetElement() throws Exception {
+
+    }
+
+    @Test
+    public void testSetElement() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRowsNumber() throws Exception {
+
+    }
+
+    @Test
+    public void testGetColumnsNumber() throws Exception {
+
+    }
+}
